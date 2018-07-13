e04d1e39f75bad2421415b647a6c8f828117af96
Julian Kingman <julian@designman.io>
2018-07-13 13:55:50 -0700

    fix false positives

--- src/index.js
+++ src/index.js
@@ -6,33 +6,45 @@
  * @param {number} sections Subsections of unit (e.g. compare 2 halves of day), default 1 (whole, not subdivided)
  */
 export default function isSameDate(d1, d2, unit = 'day', sections = 1) {
-  let answer = false;
-  const numForDate = {
-    hour: 'getUTCHours',
-    day: 'getUTCDate',
-    month: 'getUTCMonth',
-    year: 'getUTCFullYear',
-  };
-  answer = d1[numForDate[unit]]() === d2[numForDate[unit]]();
-  if (answer && sections > 1) {
-    const prevNumForDate = {
-      hour: 'getUTCMinutes',
-      day: 'getUTCHours',
-      month: 'getUTCDate',
-      year: 'getUTCMonth',
-    };
-    const unitsPerSection = {
-      hour: Math.floor(60 / sections),
-      day: Math.floor(24 / sections),
-      month: Math.floor(
+  let answer = true;
+  const numForDate = [
+    {
+      unit: 'year',
+      dateFunction: 'getUTCFullYear',
+      prevDateFunction: 'getUTCMonth',
+      unitsPerSection: Math.floor(12 / sections),
+    },
+    {
+      unit: 'month',
+      dateFunction: 'getUTCMonth',
+      prevDateFunction: 'getUTCDate',
+      unitsPerSection: Math.floor(
         new Date(d1.getUTCFullYear(), d1.getUTCMonth() + 1, 0).getUTCDate() /
           sections
       ),
-      year: Math.floor(12 / sections),
-    };
+    },
+    {
+      unit: 'day',
+      dateFunction: 'getUTCDate',
+      prevDateFunction: 'getUTCHours',
+      unitsPerSection: Math.floor(24 / sections),
+    },
+    {
+      unit: 'hour',
+      dateFunction: 'getUTCHours',
+      prevDateFunction: 'getUTCMinutes',
+      unitsPerSection: Math.floor(60 / sections),
+    },
+  ];
+  const indexOfUnit = numForDate.findIndex(u => u.unit === unit);
+  numForDate.forEach((unitInfo, index) => {
+    if (index > indexOfUnit) return;
+    answer = answer && d1[unitInfo.dateFunction]() === d2[unitInfo.dateFunction]();
+  });
+  if (answer && sections > 1) {
     answer =
-      Math.ceil(d1[prevNumForDate[unit]]() / unitsPerSection[unit]) ===
-      Math.ceil(d2[prevNumForDate[unit]]() / unitsPerSection[unit]);
+      Math.ceil(d1[numForDate[indexOfUnit].prevDateFunction]() / numForDate[indexOfUnit].unitsPerSection) ===
+      Math.ceil(d2[numForDate[indexOfUnit].prevDateFunction]() / numForDate[indexOfUnit].unitsPerSection);
   }
   return answer;
 }
